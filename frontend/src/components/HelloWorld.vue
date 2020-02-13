55902f121d3fe41bf71a1531fea00b65e0e1905b
Andres Antillon <aantillon.leiva@gmail.com>
2020-02-13 09:38:36 -0600

    Change client, server to frontend, backend

---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-card
+    class="mx-auto"
+    max-width="400"
+    tile
+  >
+  
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
