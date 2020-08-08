2018052bc920c3f105fa0eddb21a33e9da0dd9b2
Andres Antillon <aantillon.leiva@gmail.com>
2020-08-08 15:38:05 -0600

    Restructuce frontend directory

---
+++ frontend/user/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app>
+    
+    <appTopBar/>
+
+    <v-content>
+      <router-view/>
+    </v-content>
+    
+  </v-app>
+</template>
+
+<script>
+import appTopBar from "./components/layout/appTopBar"
+
+export default {
+  name: 'App',
+
+  components: {
+    appTopBar,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
